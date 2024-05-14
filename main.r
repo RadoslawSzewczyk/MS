2da88127557153d530f42cda1e46eeac7a2ae33b
RadoslawSzewczyk <radekszewczyk000@gmail.com>
2024-05-14 13:57:26 +0200

    zadanie1 poprawione

--- main.r
+++ main.r
@@ -11,31 +11,69 @@ max_freq <- max(tab)
 names(tab)[tab == max_freq]
 }
 
-zadanie1 <- function(wektor)
+zadanie_1a <- function(wektor) 
 {
-  srednia <- mean(wektor)
-  mediana <- median(wektor)
-  moda <- all_modes(wektor)
-  kwartyle <- quantile(wektor, probs = c(0.25, 0.75))
+srednia <- mean(wektor)  
+mediana <- median(wektor)
+moda <- all_modes(wektor)
+kwartyle <- quantile(wektor, probs = c(0.25, 0.75))
 
-  wariancja_obciazona <- sum((wektor - srednia)^2) / length(wektor)
-  odchylenie_standardowe_obciazone <- sqrt(wariancja_obciazona)
-  wariancja_nieobciazona <- var(wektor)
-  odchylenie_standardowe_nieobciazone <- sqrt(wariancja_nieobciazona)
-  odchylenie_przecietne_d1 <- sum(abs(wektor - srednia)) / length(wektor)
-  odchylenie_przecietne_od_mediany <- sum(abs(wektor - mediana)) / length(wektor)
-  odchylenie_cwiartkowe <- (kwartyle[2] - kwartyle[1]) 
-  wspolczynnik_zmiennosci_v <- (odchylenie_standardowe_obciazone / srednia) * 100
-  pozycyjny_wspolczynnik_zmiennosci_vq <- (odchylenie_cwiartkowe / mediana) * 100
+wariancja_obciazona <- sum((wektor - srednia)^2) / length(wektor)
+odchylenie_standardowe_obciazone <- sqrt(wariancja_obciazona)
+wariancja_nieobciazona <- var(wektor)
+odchylenie_standardowe_nieobciazone <- sqrt(wariancja_nieobciazona)
+odchylenie_przecietne_d1 <- sum(abs(wektor - srednia)) / length(wektor)
+odchylenie_przecietne_od_mediany <- sum(abs(wektor - mediana)) / length(wektor)
+odchylenie_cwiartkowe <- (kwartyle[2] - kwartyle[1]) 
+wspolczynnik_zmiennosci_v <- (odchylenie_standardowe_obciazone / srednia) * 100
+pozycyjny_wspolczynnik_zmiennosci_vq <- (odchylenie_cwiartkowe / mediana) * 100
 
 
-  skosnosc <- sum((wektor-srednia)^3)/ ( length(wektor) * odchylenie_standardowe_obciazone ^3)
-  kurtoza <- sum((wektor-srednia)^4)/ ( length(wektor) * odchylenie_standardowe_obciazone ^4)
-  eksces <- kurtoza -3
+skosnosc <- sum((wektor-srednia)^3)/ ( length(wektor) * odchylenie_standardowe_obciazone ^3)
+kurtoza <- sum((wektor-srednia)^4)/ ( length(wektor) * odchylenie_standardowe_obciazone ^4)
+eksces <- kurtoza -3
 
  
   return(list(srednia = srednia, mediana = mediana, moda = moda, kwartyle = kwartyle, wariancja_obciazona = wariancja_obciazona, odchylenie_standardowe_obciazone = odchylenie_standardowe_obciazone, wariancja_nieobciazona = wariancja_nieobciazona, odchylenie_standardowe_nieobciazone = odchylenie_standardowe_nieobciazone, odchylenie_przecietne_d1 = odchylenie_przecietne_d1, odchylenie_przecietne_od_mediany = odchylenie_przecietne_od_mediany, odchylenie_cwiartkowe = odchylenie_cwiartkowe, wspolczynnik_zmiennosci_v = wspolczynnik_zmiennosci_v, pozycyjny_wspolczynnik_zmiennosci_vq = pozycyjny_wspolczynnik_zmiennosci_vq, skosnosc = skosnosc, kurtoza = kurtoza, eksces = eksces))
 }
+
+
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
+
+szereg_rozdzielczy <- function(dane)
+{
+  ilosc_klas = sqrt(length(dane))
+  dlugosc_klasy = (max(dane) - min(dane))/ilosc_klas
+  klasy = hist(dane, breaks = ilosc_klas,main = "Histogram szeregu rozdzielczego", xlab = "Wartości", ylab = "Częstość")
+  wartosci = rep(klasy$mids, times = klasy$counts)
+  srednia = mean(wartosci)
+  mediana = median(wartosci)
+  moda = getmode(wartosci)
+  kwartyle <- quantile(wartosci, probs = c(0.25, 0.5, 0.75))
+  wariancja_obciazona <- sum((wartosci-srednia)^2)/length(wartosci)
+  wariancja_nieobciazona = var(wartosci)
+  odchylenie_standardowe_obciazone = sqrt(wariancja_obciazona)
+  odchylenie_standardowe_nieobciazone = sqrt(wariancja_nieobciazona)
+  odchylenie_przecietne_d1 = sum(abs(wartosci-srednia))/length(wartosci)
+  odchylenie_przecietne_od_mediany = sum(abs(wartosci-mediana))/length(wartosci)
+  odchylenie_cwiartkowe = kwartyle[3]-kwartyle[1]
+  wspolczynik_zmiennosc_v1 = (odchylenie_standardowe_obciazone/srednia)*100
+  pozycyjny_wspolczynik_zmiennosc_vq = (odchylenie_cwiartkowe/mediana)*100
+  
+  
+  skosnosc <- sum((wartosci-srednia)^3)/(length(wartosci)*odchylenie_standardowe_obciazone^3)
+  
+  kurtoza <- sum((wartosci-srednia)^4)/(length(wartosci)*odchylenie_standardowe_obciazone^4)
+  eksces = kurtoza -3
+  
+  return(list(srednia = srednia, mediana = mediana, moda = moda, kwartyle = kwartyle, wariancja_obciazona = wariancja_obciazona, odchylenie_standardowe_obciazone = odchylenie_standardowe_obciazone, wariancja_nieobciazona = wariancja_nieobciazona, odchylenie_standardowe_nieobciazone = odchylenie_standardowe_nieobciazone, odchylenie_przecietne_d1 = odchylenie_przecietne_d1, odchylenie_przecietne_od_mediany = odchylenie_przecietne_od_mediany, odchylenie_cwiartkowe = odchylenie_cwiartkowe, wspolczynnik_zmiennosci_v = wspolczynik_zmiennosc_v1 , pozycyjny_wspolczynnik_zmiennosci_vq =pozycyjny_wspolczynik_zmiennosc_vq , skosnosc = skosnosc, kurtoza = kurtoza, eksces = eksces))
+  
+}
+
 
 zadanie2 <- function(){
   lillie.test(wydatki_1)
@@ -104,8 +142,16 @@ zadanie5 <- function(wydatki_1, wydatki_2){
 }
 
 print("Zadanie 1 wyniki: ")
-wyniki <- zadanie1(wydatkiC)
-print(wyniki)
+a = szereg_rozdzielczy(wydatki_1)
+b = szereg_rozdzielczy(wydatki_2)
+c = zadanie_1a(wydatki_1)
+d = zadanie_1a(wydatki_2)
+
+print(c)
+print(d)
+print(a)
+print(b)
+
 
 print("Zadanie 2 wyniki: ")
 print(zadanie2())
